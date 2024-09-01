e81786a3a4ab5a4f3b3121c875a0cf24c7f0cb30
yeeyungong <51457638+yeeyungong@users.noreply.github.com>
2024-09-01 16:21:54 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "employee.cuciaa83zf99.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
